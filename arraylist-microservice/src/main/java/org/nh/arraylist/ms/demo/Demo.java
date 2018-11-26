5f7c201fe9d64e51a74a90acff6c55f20876e135
nh <yindanqing925@163.com>
2018-11-26 23:40:38 +0800

    新增测试方法;

--- arraylist-microservice/src/main/java/org/nh/arraylist/ms/demo/Demo.java
+++ arraylist-microservice/src/main/java/org/nh/arraylist/ms/demo/Demo.java
@@ -1,8 +1,13 @@
 package org.nh.arraylist.ms.demo;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
 public class Demo {
 
     public static void main(String[] args) {
         System.out.println("Hello World.");
+        System.out.println(CollectionUtils.isNotEmpty(null));
+        System.out.println(StringUtils.isNotEmpty(null));
     }
 }
