994e1dae03edacbb8389b21a4dc2729c29a55e62
yindanqing <yindanqing925@163.com>
2019-04-29 01:40:25 +0800

    搭建cloud基础架构;

--- arraylist-microservice/src/main/java/org/nh/arraylist/ms/ArrayListServiceMain.java
+++ arraylist-microservice/src/main/java/org/nh/arraylist/ms/ArrayListServiceMain.java
@@ -1,6 +1,8 @@
 package org.nh.arraylist.ms;
 
-//@Sp
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
 public class ArrayListServiceMain {
 
     public static void main(String[] args) {
